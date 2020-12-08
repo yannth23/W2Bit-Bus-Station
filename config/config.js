c84960bfa98c9875ba6e17c0fd3049b3e23c17c5
yannth23 <yannorange4@gmail.com>
2020-12-08 11:07:38 -0300

    url

--- config/config.js
+++ config/config.js
@@ -19,6 +19,6 @@ module.exports={
     "database": "dch74add36acip",
     "host": "ec2-52-201-184-16.compute-1.amazonaws.com",
     "dialect": "postgres",
-    "use_env_variable": "postgres://duaqktipzzsmgs:15f0fbe93ba72e6aafd1f98ebf041fb65bb3067b56f7dfa71f6f4b02e428f246@ec2-52-201-184-16.compute-1.amazonaws.com:5432/dch74add36acip"
+    "url": "postgres://duaqktipzzsmgs:15f0fbe93ba72e6aafd1f98ebf041fb65bb3067b56f7dfa71f6f4b02e428f246@ec2-52-201-184-16.compute-1.amazonaws.com:5432/dch74add36acip"
   }
 }
